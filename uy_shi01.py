ec385902a58145db5b0a03df75827128047aa404
Eshbo'riyev Boburbek <ilhomirmanov01@gmail.com>
2026-05-21 22:55:44 +0500

    uy ishi

---
+++ uy_shi01.py
@@ -0,0 +1,17 @@
+# matn=input("Matn kiriting:  ")
+# natija=[i for i in matn if matn.count(i)==1]
+# if natija:
+#     print(natija[-1])
+# else:
+#     print("_")
+
+
+matn=input(">>>")
+new=[float(i) for i in matn.replace(" ",",").split(",") if not i.isalpha() and "." in i]
+if new:
+    umumiy=sum(new)
+    maximum=max(new)
+    minimum=min(new)
+    print(f"Eng yuqori qiymat {maximum} ,eng kichik qiymat {minimum}, va ortacha qiymat {umumiy/len(new):.2f}")
+else:
+    print("max:0 , min:0 , average;0")
