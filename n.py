4d4a3563a0450094e1dbf64b4f6aa0c463051d4e
Eshbo'riyev Boburbek <ilhomirmanov01@gmail.com>
2026-05-21 10:27:25 +0500

    test

---
+++ n.py
@@ -0,0 +1,44 @@
+ natija=qosh(9,6532)
+# print(natija)
+
+
+# lst='w3resource'
+# natija=test(lst)
+
+# lst="london is the capital of the great britain"
+# almashtir(lst)
+
+
+# lst=["user1@gmail.com","user2@gmail.ru","user3@gmail.com"]
+# natija=solishtir(lst)
+# print(natija)
+
+
+
+
+
+# lst=[1,2,3,4]
+
+# for index,i in enumerate(lst):
+#     if index not in lst:
+#             print(index)
+#             break
+    
+# else:
+#     print(-1)
+   
+
+
+
+text="Python is great and Python is fun. Learning Python is a great experience"
+# natija=lib.harf(text)
+# print(natija)
+
+# natija=lib.soz(text)
+# print(natija)
+
+
+
+sana=input(">>>")
+natija=lib.oy(sana)
+print(natija)
